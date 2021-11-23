9f788abb02c2863949ee8c1c961623d2fdfdfbe4
Julian Calle <julian0986@gmail.com>
2021-11-23 08:58:29 -0500

    Se cambia estructura de formulario contactenos

--- src/view/contactenos.php
+++ src/view/contactenos.php
@@ -43,7 +43,7 @@ if(isset($_POST['envio_contacto'])){
     <div class="contact-wrap">
       <div class="row">
         <div class="col-md-12 column">
-          <div class="title"> <span>Estamos aqui para ayudarte</span>
+          <div class="title"> <span>Estamos aquí para ayudarte</span>
           <br/><br/>
             <h2>CONTACTENOS</h2>
           </div>
@@ -53,21 +53,21 @@ if(isset($_POST['envio_contacto'])){
         <div class="col-md-4 column">
            <div class="contact-now">
             <div class="contact"> <span><i class="fa fa-home"></i></span>
-              <div class="information"> <strong>Direccion:</strong>
+              <div class="information"> <strong>Dirección:</strong>
                 <p>Cra. 18 #78-40, Localidad de Chapinero, Bogotá</p>
               </div>
             </div>
             <!-- Contact Info -->
             <div class="contact"> <span><i class="fa fa-envelope"></i></span>
-              <div class="information"> <strong>Correo Electronico:</strong>
+              <div class="information"> <strong>Correo Electrónico:</strong>
                 <p>info@fenditrabajo.com</p>
               </div>
             </div>
             <!-- Contact Info -->
             <div class="contact"> <span><i class="fa fa-phone"></i></span>
-              <div class="information"> <strong>Telefono:</strong>
-                <p>+057 XXX XX XX XXX</p>
-                <p>+057 XXX XX XX XXX</p>
+              <div class="information"> <strong>Teléfono:</strong>
+                <p>601 621 8275</p>
+                <!-- <p>+057 XXX XX XX XXX</p> -->
               </div>
             </div>
             <!-- Contact Info -->
@@ -99,24 +99,24 @@ if(isset($_POST['envio_contacto'])){
                   <input type="text" name="cnt_apellido" id="cnt_apellido" placeholder="Apellido">
                 </div>
                 <div class="col-md-6">
-                  <input type="text" name="cnt_telefono" id="cnt_telefono" placeholder="Numero de Telefono">
+                  <input type="text" name="cnt_telefono" id="cnt_telefono" placeholder="Número de Teléfono">
                 </div>
                 <div class="col-md-6">
-                  <input  type="email" name="cnt_email" id="cnt_email" placeholder="Correco Electronico">
+                  <input  type="email" name="cnt_email" id="cnt_email" placeholder="Correco Electrónico">
                 </div>
-                <div class="col-md-6">
+               <!--  <div class="col-md-6">
                   <select class="form-control" name="cnt_tipodocumento" id="cnt_tipodocumento" required="required">
                      <option value= "" >Tipo de Documento</option>
-                     <option value="CC">Cedula de Ciudania</option>
-                     <option value="CEX">Cedula de Extrajeria</option>
-                     <option value="NUIT">Numero de Identificacion Tributaria</option>
+                     <option value="CC">Cédula de Ciudania</option>
+                     <option value="CEX">Cédula de Extrajería</option>
+                    <option value="NUIT">Número de Identificación Tributaria</option> 
                      <option value="NIT">NIT</option>
-                     <option value="SICOM" maxlength="6" >Codigo Sicom</option>
+                     <option value="SICOM" maxlength="6" >Código Sicom</option>
                    </select>
                 </div>
                 <div class="col-md-6">
                   <input  type="text" name="cnt_numerodocumento" id="cnt_numerodocumento" placeholder="Documento de Identidad">
-                </div>
+                </div> -->
                 
                 <div class="col-md-12">
                   <textarea rows="4" name="cnt_solicitud" id="cnt_solicitud" placeholder="Solicitud"></textarea>
